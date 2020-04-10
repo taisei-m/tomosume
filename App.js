dc2bc2f4208a55ca61d077128a7dca8a16f9e030
oxyu8 <liberalist0449@gmail.com>
2020-04-10 17:53:58 +0900

    add react-navigation

--- App.js
+++ App.js
@@ -1,84 +1,24 @@
 import React from 'react';
-import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
+import { StyleSheet, Button, Text, View } from 'react-native';
+import { createAppContainer } from 'react-navigation';
+import { createStackNavigator } from 'react-navigation-stack';
+import HomeScreen from './components/Home'
+import DetailsScreen from './components/Login'
 
-export default function App() {
-  // state = {
-  //   userId: '',
-  //   password: '',
-  // }
-  return (
-    <View style={styles.container}>
-    <Text style={styles.logo}>Tomosume</Text>
-    <View style={styles.inputView} >
-      <TextInput  
-        style={styles.inputText}
-        placeholder="userId..." 
-        placeholderTextColor="white"
-        onChangeText={text => this.setState({userId:text})}/>
-    </View>
-    <View style={styles.inputView} >
-      <TextInput  
-        secureTextEntry
-        style={styles.inputText}
-        placeholder="Password..." 
-        placeholderTextColor="white"
-        onChangeText={text => this.setState({password:text})}/>
-    </View>
-    <TouchableOpacity>
-      <Text style={styles.forgot}>Forgot Password?</Text>
-    </TouchableOpacity>
-    <TouchableOpacity style={styles.loginBtn}>
-      <Text style={styles.loginText}>LOGIN</Text>
-    </TouchableOpacity>
-    <TouchableOpacity>
-      <Text style={styles.loginText}>Signup</Text>
-    </TouchableOpacity>
-  </View>
-  );
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#242E37',
-    alignItems: 'center',
-    justifyContent: 'center',
+const RootStack = createStackNavigator(
+  {
+    Home: HomeScreen,
+    Details: DetailsScreen,
   },
-  logo:{
-    fontWeight:"bold",
-    fontSize:50,
-    color:"white",
-    marginBottom:40
-  },
-  inputView:{
-    width:"80%",
-    backgroundColor: 'grey',
-    opacity: .3,
-    borderRadius:25,
-    height:50,
-    marginBottom:20,
-    justifyContent:"center",
-    padding:20
-  },
-  inputText:{
-    height:50,
-    color:"white"
-  },
-  forgot:{
-    color:"white",
-    fontSize:11
-  },
-  loginBtn:{
-    width:"80%",
-    backgroundColor:"#3E48E0",
-    borderRadius:25,
-    height:50,
-    alignItems:"center",
-    justifyContent:"center",
-    marginTop:40,
-    marginBottom:10
-  },
-  loginText:{
-    color:"white"
+  {
+    initialRouteName: 'Home',
   }
-});
+);
+
+const AppContainer = createAppContainer(RootStack);
+
+export default class App extends React.Component {
+  render() {
+    return <AppContainer />;
+  }
+}
---
+++ components/Home.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import { StyleSheet, Button, Text, View } from 'react-native';
+
+export default class HomeScreen extends React.Component {
+    render(){
+        return (
+            <View style={styles.container}>
+            <Text>Open up App.js to start working on your app!</Text>
+            <Text>Hello World!</Text>
+            <Button
+                title="Go to Details"
+                onPress={() => this.props.navigation.navigate('Details')}
+            />
+            </View>
+        );
+    }
+}
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        backgroundColor: '#fff',
+        alignItems: 'center',
+        justifyContent: 'center',
+    },
+});
