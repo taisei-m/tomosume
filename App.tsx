8aca514fdd375de8e7e35c7b420feb8caa1f4324
oxyu8 <liberalist0449@gmail.com>
2020-04-29 00:17:57 +0900

    :sparkles: New features.
    typescript導入した。
    まだ導入しただけなので、これから調べつつコードを変更していくことにする

--- App.tsx
+++ App.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
 import { AppRegistry } from 'react-native';
+//@ts-ignore
 import Router from './src/Routes/index'
 import { Provider } from 'unstated';
-import {decode, encode} from 'base-64'
-if (!global.btoa) {  global.btoa = encode }
-if (!global.atob) { global.atob = decode }
+// import {decode, encode} from 'base-64'
+// // if (!global.btoa) { global.btoa = encode }
+// // if (!global.atob) { global.atob = decode }
 
 
 export default function App() {
