3a4d573299ba055d873e57b4e0579789a425aa81
oxyu8 <liberalist0449@gmail.com>
2020-04-30 18:47:50 +0900

    :wrench: fixed some files
    buttonをコンポーネント 化した

---
+++ src/components/ShareButton.tsx
@@ -0,0 +1,49 @@
+import React from 'react';
+import { Button } from 'react-native-elements';
+//@ts-ignore
+import firebase from '../../firebase';
+
+interface ShareButtonProps {
+    shopName: string
+    favoriteMenu: string,
+    price: string,
+    category: string,
+    finalRating: number,
+    latitude: number,
+    longitude: number,
+    buttonTitle: string,
+    buttonType?: string,
+}
+
+const ShareButton = (props: ShareButtonProps) => {
+    const share = () => {
+        const postShopData = firebase.firestore().collection('postShopData')
+        postShopData.add({
+            shopName: props.shopName,
+            favoriteMenu: props.favoriteMenu,
+            price: props.price,
+            category: props.category,
+            rating: props.finalRating,
+            createdAt: new Date(),
+            latitude: props.latitude,
+            longitude: props.longitude,
+        })
+        .then(function() {
+            console.log('success')
+        })
+        .catch(function(error: any) {
+            console.log(error)
+        })
+    }
+    return(
+        <>
+            <Button
+                title={props.buttonTitle}
+                type={props.buttonType}
+                onPress={share}
+            />
+        </>
+    )
+}
+
+export default ShareButton
--- src/screens/Post.tsx
+++ src/screens/Post.tsx
@@ -1,27 +1,50 @@
 import React from 'react';
 import { useState, useEffect } from 'react';
 import { StyleSheet, View, } from 'react-native';
-import { Button, AirbnbRating} from 'react-native-elements';
+import { AirbnbRating} from 'react-native-elements';
 import { Dropdown } from 'react-native-material-dropdown';
 //@ts-ignore
 import firebase from '../../firebase'
 import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import InputText from '../components/InputText';
+import ShareButton from '../components/ShareButton'
 
-interface Props {}
-
-const Post: React.FC<Props> = () => {
+const Post = () => {
     useEffect(() => {
-        permit();
+        (async function () {
+            let locationResult = await Permissions.askAsync(Permissions.LOCATION);
+            inputResult()
+            console.log(locationResult.status)
+            if (locationResult.status !== 'granted') {
+                alert('noo')
+            } else if (locationResult.status === 'granted') {
+                let location = await Location.getCurrentPositionAsync({});
+                console.log('good')
+                setLatitude(Number(JSON.stringify(location.coords.latitude)))
+                setLongitude(Number(JSON.stringify(location.coords.longitude)))
+            }
+        }());
     }, [])
 
     let finalRating = 0
-    const [locationResultLatitude, setResultLatitude] = useState<number>()
-    const [locationResultLongitude, setResultLongitude] = useState<number>()
-    const [locationResult, permitResult] = useState<string>()
-    const [selectedCategory, selectItem] = useState<string>()
+    const [shopName, changeShop] = useState<string>('')
+    const [favoriteMenu, changeFavorite] = useState<string>('')
+    const [price, changePrice] = useState<string>('')
+    const [locationResultLatitude, setResultLatitude] = useState<number>(0)
+    const [locationResultLongitude, setResultLongitude] = useState<number>(0)
+    const [locationResult, permitResult] = useState<string>('')
+    const [selectedCategory, selectItem] = useState<string>('')
 
+    const changeShopName = (text: string) => {
+        changeShop(text)
+    }
+    const changeFavoriteMenu = (text: string) => {
+        changeFavorite(text)
+    }
+    const changeMenuPrice = (price: string) => {
+        changePrice(price)
+    }
     const ratingCompleted = (rating: number) => {
         finalRating = rating
     }
@@ -37,54 +60,6 @@ const Post: React.FC<Props> = () => {
     const setLongitude = (longitude: number) => {
         setResultLongitude(longitude)
     }
-
-    const share = () => {
-        const postShopData = firebase.firestore().collection('postShopData')
-        postShopData.add({
-            shopName: shopName,
-            favoriteMenu: favoriteMenu,
-            price: price,
-            category: selectedCategory,
-            rating: finalRating,
-            createdAt: new Date(),
-            latitude: locationResultLatitude,
-            longitude: locationResultLongitude,
-        })
-        .then(function() {
-            console.log('success')
-        })
-        .catch(function(error: any) {
-            console.log(error)
-        })
-    }
-
-    const permit = async () => {
-        let locationResult = await Permissions.askAsync(Permissions.LOCATION);
-        inputResult()
-        console.log(locationResult.status)
-        if (locationResult.status !== 'granted') {
-            alert('noo')
-        } else if (locationResult.status === 'granted') {
-            let location = await Location.getCurrentPositionAsync({});
-            console.log('good')
-            setLatitude(Number(JSON.stringify(location.coords.latitude)))
-            setLongitude(Number(JSON.stringify(location.coords.longitude)))
-        }
-    }
-    const [shopName, changeShop] = useState<string>('')
-    const [favoriteMenu, changeFavorite] = useState<string>('')
-    const [price, changePrice] = useState<string>('')
-
-    const changeShopName = (text: string) => {
-        changeShop(text)
-    }
-    const changeFavoriteMenu = (text: string) => {
-        changeFavorite(text)
-    }
-    const changeMenuPrice = (price: string) => {
-        changePrice(price)
-    }
-
     return (
         <View style={styles.container}>
             <InputText 
@@ -102,36 +77,41 @@ const Post: React.FC<Props> = () => {
                 value={price}
                 change={changeMenuPrice}
             />
-        <View style={{alignContent: 'center', marginHorizontal: 60 }}>
-            <Dropdown
-                label='カテゴリー'
-                data={
-                    [
-                        {value: '居酒屋',},
-                        {value: 'カフェ',}, 
-                        {value: 'ランチ',}, 
-                        {value: 'ディナー',}
-                    ]
-                }
-                value={selectedCategory}
-                onChangeText={selectCategory}
+            <View style={{alignContent: 'center', marginHorizontal: 60 }}>
+                <Dropdown
+                    label='カテゴリー'
+                    data={
+                        [
+                            {value: '居酒屋',},
+                            {value: 'カフェ',}, 
+                            {value: 'ランチ',}, 
+                            {value: 'ディナー',}
+                        ]
+                    }
+                    value={selectedCategory}
+                    onChangeText={selectCategory}
+                />
+            </View>
+            <AirbnbRating
+                count={5}
+                reviews={["1/5", "2/5", "3/5", "4/5", "5/5",]}
+                defaultRating={0}
+                size={30}
+                onFinishRating={ratingCompleted}
             />
-        </View>
-        <AirbnbRating
-            count={5}
-            reviews={["1/5", "2/5", "3/5", "4/5", "5/5",]}
-            defaultRating={0}
-            size={30}
-            onFinishRating={ratingCompleted}
-        />
-        <View style={{alignContent: 'center', marginHorizontal: 60, marginTop: 30 }}>
-            <Button
-            style={styles.shareButton}
-            title="シェア"
-            type="outline"
-            onPress={share}
-            />
-        </View>
+            <View style={{alignContent: 'center', marginHorizontal: 60, marginTop: 30 }}>
+                <ShareButton
+                    buttonTitle='シェア'
+                    buttonType="outline"
+                    shopName={shopName}
+                    favoriteMenu={favoriteMenu}
+                    price={price}
+                    category={selectedCategory}
+                    finalRating={finalRating}
+                    latitude={locationResultLatitude}
+                    longitude={locationResultLongitude}
+                />
+            </View>
         </View>
     );
 }
@@ -164,7 +144,4 @@ inputText:{
 inputShopName: {
     width: 30
 },
-shareButton: {
-
-}
 })
