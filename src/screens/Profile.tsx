43a2b5a74c73ae5531677b6f0f09f85aff47f5e8
oxyu8 <liberalist0449@gmail.com>
2020-05-31 22:19:34 +0900

    :recycle: Refactoring code.
    コメントを追加した

--- src/screens/Profile.tsx
+++ src/screens/Profile.tsx
@@ -13,13 +13,14 @@ import {userDataDocResponse} from '../types/types'
 import {userReviewsType} from '../types/types'
 
 const Profile = (props: any) => {
-  const [userName, setUserName] = useState<string>()
-  const [followee, setFollowee] = useState<number>(0)
-  const [follower, setFollower] = useState<number>(0)
-  const [postNumber, setPostNumber] = useState<number>(0)
-  const [allReviews, setAllReviews] = useState<userReviewsType>([])
-  const [userIcon, setUserIcon] = useState<string>();
+  const [_userName, setUserName] = useState<string>()
+  const [_followee, setFollowee] = useState<number>(0)
+  const [_follower, setFollower] = useState<number>(0)
+  const [_postNumber, setPostNumber] = useState<number>(0)
+  const [_allReviews, setAllReviews] = useState<userReviewsType>([])
+  const [_userIcon, setUserIcon] = useState<string>();
 
+  // ユーザが投稿したレビューの一覧と投稿数を取得
   useEffect(() => {
     const userId = props.globalState.state.userData.uid
     const userFirestoreDocument = firebase.firestore().collection('userList').doc(userId)
@@ -35,6 +36,7 @@ const Profile = (props: any) => {
         setAllReviews(userReviews)
       })
   }, [])
+  // ユーザーのアイコン画像を取得
   useEffect(() => {
     (async () => {
       const userId = props.globalState.state.userData.uid
@@ -46,16 +48,17 @@ const Profile = (props: any) => {
       userProfileDocument.iconURL != 'test-url' ? setUserIcon(userProfileDocument.iconURL) : setUserIcon('file:///Users/oxyu8/Downloads/okuse_yuya.jpg')
     })();
   }, [])
-
+  // ユーザの名前を取得
   useEffect(() => {
     const userId = props.globalState.state.userData.uid
     firebase.firestore().collection('userList').doc(userId)
     .get().then(function(doc) {
       let userProfileData = doc.data() as userDataDocResponse
+      console.log(userProfileData)
       setUserName(userProfileData.userName)
     })
   },[])
-
+  // ユーザのフォロワーを取得
   useEffect(() => {
     const userId = props.globalState.state.userData.uid
     let followeeArray:string[] = []
@@ -68,15 +71,15 @@ const Profile = (props: any) => {
       let followeeNumber: number = followeeArray.length-1
       setFollowee(followeeNumber)
   })
-//なんで俺any型使ってるの？？
   },[])
+  // ユーザのフォローしている人を取得
   useEffect(() => {
     const userId = props.globalState.state.userData.uid
     let followerArray:string[] = []
     db.collection('userList').doc(userId).collection('follower')
     .get()
-    .then(function(querySnapshot:any) {
-      querySnapshot.forEach(function(doc:any) {
+    .then(function(querySnapshot) {
+      querySnapshot.forEach(function(doc) {
         followerArray.push(doc.id)
       })
       let followerNumber: number = followerArray.length-1
@@ -92,13 +95,13 @@ const Profile = (props: any) => {
   const toFollowerList = () => {
     props.navigation.navigate('followerList')
   }
-  const imageInput = (gotresult: string) =>{
-    setUserIcon(gotresult);
+  const imageInput = (url: string) =>{
+    setUserIcon(url);
   }
   const changeIcon = async() => {
-    const _result = await changeIconUrl()
-    await setIconToFirestore(_result);
-    imageInput(_result);
+    const iconUrl = await changeIconUrl()
+    await setIconToFirestore(iconUrl);
+    imageInput(iconUrl);
   }
   const changeIconUrl = async():Promise<string> => {
     let result = await selectIconPicture() as pickerResult
@@ -142,13 +145,13 @@ const Profile = (props: any) => {
               onPress={changeIcon}
             >
               <Image
-                source={{ uri: userIcon }}
+                source={{ uri: _userIcon }}
                 style = {styles.userIcon}
               />
             </TouchableOpacity>
           </View>
           <View style={{alignItems: 'center', marginTop: 10}}>
-            <Text style={styles.userName}>{userName}</Text>
+            <Text style={styles.userName}>{_userName}</Text>
           </View>
         </View>
         <View style={{marginLeft: 30}}>
@@ -160,17 +163,17 @@ const Profile = (props: any) => {
             }}
           >
             <ProfileNumber
-              number={postNumber}
+              number={_postNumber}
               itemName='post'
             />
             <ProfileNumber
-              number={follower}
+              number={_follower}
               itemName="フォロー"
               press={toFollowerList}
               centerClass={{width: 50, height: 50, marginHorizontal: 30}}
             />
             <ProfileNumber
-              number={followee}
+              number={_followee}
               itemName="フォロワー"
               press={toFolloweeList}
             />
@@ -190,7 +193,7 @@ const Profile = (props: any) => {
       </View>
       <SafeAreaView style={styles.list}>
         <FlatList
-          data={allReviews}
+          data={_allReviews}
           renderItem={
             ({ item }) =>
               <ProfileReviews
