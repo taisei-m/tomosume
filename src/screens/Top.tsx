0506c676cd7598384258d551c85e54ecf206b7ec
oxyu8 <liberalist0449@gmail.com>
2020-06-06 15:54:39 +0900

    :lipstick: Updating the UI and style files.
    トップ画面のUIを修正した。
    デバイスに依存せずに表示されるはず

--- src/screens/Top.tsx
+++ src/screens/Top.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, FlatList } from 'react-native';
 import {db} from '../../firebaseConfig'
-import Item from '../components/Item';
+import ListItem from '../components/ListItem';
 import { Subscribe } from 'unstated';
 import GlobalStateContainer from '../containers/GlobalState';
 import {ReviewDocResponse} from '../types/types'
@@ -68,10 +68,9 @@ const Top = (props) => {
     return(
         <View style={styles.container}>
             <FlatList
-            style={styles.list}
             data={allReviews}
             renderItem={
-                ({ item }) => <Item
+                ({ item }) => <ListItem
                                 id={item.shopId}
                                 title={item.shopName}
                                 category={item.category}
@@ -105,12 +104,7 @@ export default ProfileWrapper
 
 const styles = StyleSheet.create({
     container: {
-        flex: 1,
-        paddingVertical: 10,
         backgroundColor: 'white',
-        alignItems: 'center',
-    },
-    list: {
-        marginTop: 30
+        paddingVertical: 20
     },
 });
