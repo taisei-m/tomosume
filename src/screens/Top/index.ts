c3eab05c344a78c5283f40632c17c6ef692bfb94
oxyu8 <liberalist0449@gmail.com>
2020-08-08 16:04:52 +0900

    :recycle: Refactoring code.
    firestoreの型を記述した

--- src/screens/Top/index.ts
+++ src/screens/Top/index.ts
@@ -2,6 +2,13 @@ import {db} from '../../../firebaseConfig'
 import firebase from '../../../firebaseConfig'
 import { ReviewDocResponse } from 'src/types/types'
 
+type User = {
+    followee: firebase.firestore.CollectionReference,
+    follower: firebase.firestore.CollectionReference,
+    iconURL: string,
+    uid: string,
+    userName: string
+}
 export const fetchFolloweeIds = async(uid: string):Promise<string[]> => {
     const querySnapshot = await db.collection('userList').doc(uid).collection('followee').get()
     const followeeIds = querySnapshot.docs.map(doc => {
@@ -10,18 +17,18 @@ export const fetchFolloweeIds = async(uid: string):Promise<string[]> => {
     return followeeIds
 }
 
-export const convertToReference = async(followeeIds: string[]):Promise<any> => {
-    let reference: firebase.firestore.DocumentReference
+export const convertToReference = async(followeeIds: string[]):Promise<firebase.firestore.DocumentReference<User>[]> => {
+    let reference: firebase.firestore.DocumentReference<User>
     followeeIds = followeeIds.filter(n => n != 'first')
     const convertedFollowees = followeeIds.map(uid => {
-        reference = db.collection('userList').doc(uid)
+        reference = db.collection('userList').doc(uid) as firebase.firestore.DocumentReference<User>
         return reference
     })
     return convertedFollowees
 }
 
-export const fetchReviews = async(queryDocsSnapshot: any):Promise<any> => {
-    const reviews = await Promise.all(queryDocsSnapshot.map(async (item) => {
+export const fetchReviews = async(queryDocsSnapshot: any):Promise<ReviewDocResponse[]> => {
+    const reviews: ReviewDocResponse[] = await Promise.all(queryDocsSnapshot.map(async (item) => {
         let review = item.data() as ReviewDocResponse
         review.key = item.id
         const userProfile = await (review.user).get()
