de9aca5a0d1a416c3d03f5e382c060625973eeb3
oxyu8 <liberalist0449@gmail.com>
2020-09-15 14:25:35 +0900

    console.log消した

--- src/screens/Top/index.ts
+++ src/screens/Top/index.ts
@@ -27,7 +27,6 @@ export const fetchReviews = async (
 	const reviews: ReviewsDocResponse = await Promise.all(
 		queryDocsSnapshot.map(async (item) => {
 			const review = item.data() as ReviewDocResponse;
-			console.log(review.shopAddress);
 			review.key = item.id;
 			const userProfile = await review.user.get();
 			review.userName = userProfile.get('userName');
