98bfa7391481896630f1e7ca5cd376d19c52a07c
Taisei Watanabe <yomichi.o.terasu.house@gmail.com>
2021-02-20 10:54:39 +0900

    descriptionのコンポーネント化
    descriptionの詳細のコンポーネントを三項演算子で出力するようにした

--- src/components/Pindescription.tsx
+++ src/components/Pindescription.tsx
@@ -1,28 +1,38 @@
-import { PinFunc } from '@/types/types';
+import { PinFunc, ShopDocResponse } from '@/types/types';
 import React, { useState, useEffect } from 'react';
 import { View, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
 
+const PinDescriptionDetail = (props: {shopDoc: ShopDocResponse}) => {
+    return(
+        <View>
+            <View style={styles.description}>
+                <Text>{props.shopDoc.shopName}</Text>
+                <Text>{props.shopDoc.address}</Text>
+            </View>
+        </View>
+    )
+}
 
 const Pindescription = (props: PinFunc) =>{ 
 
-    const [_descriptionVisible, setDescriptionVisible] =useState<any>('none');
+    const [_descriptionVisible, setDescriptionVisible] =useState<boolean>(false);
 
     useEffect(() => {
         if( props.openedPinId == props.shopDoc.id ){
-            setDescriptionVisible('');
+            setDescriptionVisible(true);
         } else {
-            setDescriptionVisible('none');
+            setDescriptionVisible(false);
         }
-	}, [props.openedPinId]);
+    }, [props.openedPinId]);
+    
+   
 
     return(
         <View>
-            <View style={{display: _descriptionVisible}}>
-                <View style={styles.description}>
-                    <Text>{props.shopDoc.shopName}</Text>
-                    <Text>{props.shopDoc.address}</Text>
-                </View>
-            </View>
+            {(props.openedPinId == props.shopDoc.id)
+                ? <PinDescriptionDetail shopDoc={props.shopDoc} />
+                : null
+            }
             <TouchableOpacity 
                 onPress={
                     () => {
